8bd300f61f77ff8e35e07340f9c2728033ea2775
Robert Dubrovskis <robert.dubrovskis@gmail.com>
2018-09-14 10:47:44 +0200

    added Comment model

---
+++ Stipendium/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stipendium.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public virtual ApplicationUser Commenter { get; set; }
+        public virtual Stiftelse Stiftelse { get; set; }
+        public string Body { get; set; }
+        public DateTime CommentDate { get; set; }
+    }
+}
